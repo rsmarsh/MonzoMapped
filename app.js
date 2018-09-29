b7ab23b58c645cde08565d05d7bb18100c588425
rsmarsh <missingmarshall@gmail.com>
2018-09-29 21:35:06 +0100

    Added a basic test to open Monzo's OAuth2 page Redirect URI currently set to a unsupported local host address

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const authoriser = require('./private/authoriser');
 const express = require('express');
 const app = express();
 const port = 3001;
+
 let visitorCounter = 0;
 
 let logVisitor = function(req,res,next) {
@@ -9,7 +11,9 @@ let logVisitor = function(req,res,next) {
 };
 
 // only server static files within the public folder or lower
-app.use('/', logVisitor,  express.static(__dirname + '/public'))
+app.get('/', logVisitor,  express.static(__dirname + '/public'))
+
+app.get('/monzo', authoriser.connectMonzo);
 
 
 app.listen(port);
